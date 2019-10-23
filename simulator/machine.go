77c9530b5985e2058cd18e6677b019bf1eb20369
Kazushi Kitaya <kazushikitaya@gmail.com>
2019-10-23 11:20:27 +0900

    add cache for Machine.findAddress()

--- simulator/machine.go
+++ simulator/machine.go
@@ -21,21 +21,28 @@ type Machine struct {
 	Memory            []ValueWithLabel
 	ProgramCounter    int
 	ConditionRegister bool
+	findAddressCache  map[Label]int
 }
 
 // NewMachine creates a Machine instance with memory of the specified size.
 func NewMachine(initialMemorySize int) Machine {
 	return Machine{
-		IntRegisters:   make(map[string]int),
-		FloatRegisters: make(map[string]float32),
-		Memory:         make([]ValueWithLabel, initialMemorySize),
+		IntRegisters:     make(map[string]int),
+		FloatRegisters:   make(map[string]float32),
+		Memory:           make([]ValueWithLabel, initialMemorySize),
+		findAddressCache: make(map[Label]int),
 	}
 }
 
 // FindAddress iterates through the memory and returns the label's matching address.
 func (m *Machine) FindAddress(label Label) (int, error) {
+	if address, ok := m.findAddressCache[label]; ok {
+		return address, nil
+	}
+
 	for i, d := range m.Memory {
 		if d.Label == label {
+			m.findAddressCache[label] = i
 			return i, nil
 		}
 	}
